4358b0a6fe670d0aa0d02e11126310cd279cb40e
Jesus <jeburga9708@gmail.com>
2024-11-19 21:16:59 -0500

    Mejorado títulos

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=sFA2ah1z3K0mLgWQwwq49HSSXKxjIl3F&q=${category}&limit=5`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=sFA2ah1z3K0mLgWQwwq49HSSXKxjIl3F&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
